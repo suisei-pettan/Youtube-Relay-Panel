f55335a6b5fe954e6fd78c876fece6edcd6817a4
冰糖主人的电子宠物 <44109309+BTIOWaifu@users.noreply.github.com>
2023-04-11 14:42:10 +0800

    修复指令执行

--- stream.mjs
+++ stream.mjs
@@ -1,62 +1,34 @@
 import { readFileSync } from "fs";
-
 import { createServer } from "https";
-
 import { Server } from "socket.io";
-
 import { exec } from 'child_process';
 
 const httpServer = createServer({
-
   key: readFileSync("/www/server/panel/ssl/privateKey.pem"),
-
   cert: readFileSync("/www/server/panel/ssl/certificate.pem")
-
 });
 
 const io = new Server(httpServer, { /* options */
-
   cors: {
-
     origin: "https://btiowaifu.github.io"
-
   }
-
 });
 
 io.on("connection", (socket) => {
-
   socket.on('pushurl', (data) => {
-
     if (!data.url.includes('share')) {
-
       socket.emit('not-standard-link', '请使用YouTube分享链接');
-
     } else {
-
       console.log(readFileSync('/www/urls.txt', 'utf8'));
-
       const urls = readFileSync('/www/urls.txt', 'utf8');
-
       if (urls.includes(data.url)) {
-
         socket.emit('broadcasted', '此直播已转播');
-
       } else {
-
         //const command = `python3 /www/streambackend.py ${data.url} ${data.stream_link}`;
-
         exec('python3 /www/streambackend.py ' + data.url + ' ' + data.stream_link);
-
         // execute command
-
       } 
-
     }
-
   });
-
 });
-
 httpServer.listen(2083);
-
