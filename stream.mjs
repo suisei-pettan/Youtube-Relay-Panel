7e14f65a291e3f68e9e41ad48598dc5e6adf7b10
冰糖主人的电子宠物 <44109309+BTIOWaifu@users.noreply.github.com>
2023-04-12 11:20:47 +0800

    关闭推流，后端支持

--- stream.mjs
+++ stream.mjs
@@ -19,11 +19,11 @@ io.on("connection", (socket) => {
     var pattern = /.*(吹雪|笨蛋).*/;
     var danma = JSON.parse(arg);
     if(pattern.test(danma.text)){
-	console.log(pattern.test(danma.text));
+      console.log(pattern.test(danma.text));
     }else{
-	console.log(danma.text);
-    var channel=danma.ch;
-    io.emit("danmaku"+channel, arg);
+      console.log(danma.text);
+      var channel=danma.ch;
+      io.emit("danmaku"+channel, arg);
     }
   });
   socket.on('pushurl', (data) => {
@@ -38,7 +38,29 @@ io.on("connection", (socket) => {
         //const command = `python3 /www/streambackend.py ${data.url} ${data.stream_link}`;
         console.log('python3 /www/streambackend.py ' + data.url + ' ' +'"'+ data.stream_link+'"');
         exec('python3 /www/streambackend.py ' + data.url + ' ' +'"'+ data.stream_link+'"');
-        // execute command
+
+        // 添加关闭推流监听
+        socket.on('closepush', (close_data) => {
+          const stream_link_pid = readFileSync('/www/pid.txt', 'utf8');
+          const stream_link_pid_array = stream_link_pid.trim().split('\n').map(item => item.trim().split(','));
+          const pid_to_kill = stream_link_pid_array.find(item => item[0] === close_data.stream_link);
+
+          if (pid_to_kill) {
+            const pid = pid_to_kill[1];
+            exec(`kill ${pid}`, (error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+              console.log(`stdout: ${stdout}`);
+              console.error(`stderr: ${stderr}`);
+            });
+
+            socket.emit('push-closed', '推流已关闭');
+          } else {
+            socket.emit('push-not-started', '该链接的推流未开启');
+          }
+        });
       } 
     }
   });
