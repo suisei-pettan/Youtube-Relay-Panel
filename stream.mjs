483151ad6cfdf9db33ba76ded0d44372520f091d
冰糖主人的电子宠物 <44109309+BTIOWaifu@users.noreply.github.com>
2023-04-14 18:54:41 +0800

    尝试修复停止直播

--- stream.mjs
+++ stream.mjs
@@ -3,6 +3,8 @@ import { createServer } from "https";
 import { Server } from "socket.io";
 import { exec } from "child_process";
 
+global.urls=[];
+
 const httpServer = createServer({
   key: readFileSync("/www/server/panel/ssl/privateKey.pem"),
   cert: readFileSync("/www/server/panel/ssl/certificate.pem"),
@@ -36,40 +38,88 @@ io.on("connection", (socket) => {
     if (!data.url.includes("you")) {
       socket.emit("not-standard-link", "请使用YouTube分享链接");
     } else {
-      console.log(readFileSync("/www/urls.txt", "utf8"));
-      const urls = readFileSync("/www/urls.txt", "utf8");
-      if (urls.includes(data.url)) {
+      if (global.urls.includes(data.url)) {
         socket.emit("broadcasted", "此直播已转播");
       } else {
+        global.urls.push(data.url);
         console.log('python3 /www/streambackend.py ' + data.url + ' ' +'"'+ data.stream_link+'"');
         exec('python3 /www/streambackend.py ' + data.url + ' ' +'"'+ data.stream_link+'"', (error, stdout, stderr) => {
-          if (error) {
+        if (error) {
             console.log(`error: ${error.message}`);
             return;
-          }
-          if (stderr) {
+        }
+        if (stderr) {
             console.log(`stderr: ${stderr}`);
             return;
-          }
-          console.log(`stdout: ${stdout}`);
+        }
+
+  // 获取Python进程PID和进程p的PID
+        const [python_pid, p_pid] = stdout.trim().split('\n')[0].split(' ');
+
+        console.log(`Python进程PID: ${python_pid}`);
+        console.log(`进程p的PID: ${p_pid}`);
+        if (!global.arr) {
+  global.arr = [[python_pid, p_pid, data.stream_link]];
+} else {
+  global.arr.push([python_pid, p_pid, data.stream_link]);
+}
+
+console.log(global.arr);
+
+
+  // 在这里可以将PID存储到二维数组的第一第二列中，具体方式取决于您的代码逻辑
         });
+
       }
     }
   });
   socket.on("close-stream", (value) => { // 监听 "close-stream" 事件
-    const pidTxt = readFileSync("/www/pid.txt", "utf8").trim();
-    for(var i =0;i<=pidTxt.length;i++){
-        if(pidTxt[i][0]==value.stream_link){
-            exec('kill -9 ' + pidTxt[i][1]);
-            pidTxt.splice(i,1);
-            fs.writeFile('/www/pid.txt', pidTxt);
-        }else{
-            console.log("无目标")
+  if (global.arr) {
+  const pidIndex = 1;
+  const streamLinkIndex = 2;
+
+  for (let i = global.arr.length - 1; i >= 0; i--) {
+    const row = global.arr[i];
+    if (row[streamLinkIndex] === value.stream_link) {
+      // 找到包含 value.stream_link 的行，杀死其前两列中的PID
+      const pythonPid = row[0];
+      const pPid = row[pidIndex];
+      exec(`kill -9 ${pythonPid} ${pPid}`, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
         }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+
+        // 从数组中删除这一行
+        global.arr.splice(i, 1);
+      });
     }
-    console.log(`killing process ${pidToKill}`);
-    
-  });
+  }
+
+  console.log(global.arr);
+} else {
+  console.log('global.arr is undefined or null');
+}
+if (global.urls) {
+  const index = global.urls.indexOf(value.url);
+  if (index !== -1) {
+    global.urls.splice(index, 1);
+    console.log(`已删除${value.url}`);
+  } else {
+    console.log(`${value.url}在global.urls中不存在`);
+  }
+} else {
+  console.log(`global.urls不存在或为null`);
+}
+
+});
+
+
 });
 
 httpServer.listen(2083);
--- streambackend.py
+++ streambackend.py
@@ -3,6 +3,8 @@ import sys
 import os
 import time
 import json
+import ast
+
 
 def main():
     # 获取启动参数
@@ -15,33 +17,24 @@ def main():
 
     while True:
         # 执行 ytdl-patched -g 获取视频链接
-        ytdl_patched_output = subprocess.check_output(f"ytdl-patched -g {youtube_url}", shell=True)
+        ytdl_patched_output = subprocess.check_output(
+            f"ytdl-patched -g {youtube_url}", shell=True)
         source_live = ytdl_patched_output.decode().strip()
 
         # 执行 ffmpeg
-        ffmpeg_command = f'ffmpeg -loglevel info -i {source_live} -c:v copy -c:a aac -b:a 600k -ar 44100 reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 10 -strict -2 -f flv \"{channel_number}\"';
+        ffmpeg_command = f'ffmpeg -loglevel quiet -i {source_live} -c:v copy -c:a aac -b:a 600k -ar 44100 -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 10 -strict -2 -f flv \"{channel_number}\"'
         p = subprocess.Popen(ffmpeg_command, shell=True)
-
-        # 将 channel_number 和进程的pid作为二维数组的两个值存入 /www/pid.txt
-        pid_arr = [[channel_number, p.pid]]
-        with open('/www/pid.txt', 'a') as pid_file:
-            json.dump(pid_arr, pid_file)
-            pid_file.write('\n')
-
+        return (os.getpid(),p.pid)
         # 等待5.5小时后停止进程
         time.sleep(19800)
-        p.terminate()
-        p.communicate()
 
         # 删除已经处理过的 youtube_url
-        with open("/www/urls.txt", "r") as file:
-            urls = file.readlines()
-        with open("/www/urls.txt", "w") as file:
-            for url in urls:
-                if url.strip() != youtube_url:
-                    file.write(url)
-
-    return
+    with open("/www/urls.txt", "r") as file:
+        urls = file.readlines()
+    with open("/www/urls.txt", "w") as file:
+        for url in urls:
+            if url.strip() != youtube_url:
+                file.write(url)
 
 if __name__ == "__main__":
     main()
