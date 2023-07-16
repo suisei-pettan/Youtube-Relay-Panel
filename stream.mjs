e223820908f8ad02f50d34efcea2b8b4e9e4faad
我和神楽七奈啵过嘴 <2095532804@qq.com>
2023-07-16 16:10:44 +0800

    更换apikey读取方式

--- stream.mjs
+++ stream.mjs
@@ -13,7 +13,7 @@ const httpServer = createServer({
 const io = new Server(httpServer, {
     /* options */
     cors: {
-        origin: "https://btiowaifu.github.io",
+        origin: "https://suisei-pettan.github.io/",
     },
 });
 
